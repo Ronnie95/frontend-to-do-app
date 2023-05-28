206f9c1b279cea551bc2f38faeab171f589a1a5a
Ronnie95 <t_daron@yahoo.com>
2023-05-28 11:47:11 -0400

    updates

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
+// import { ReactDOM } from 'react-router-dom';
+// import {BrowserRouter, Routes, Route} from "react-router-dom"
 import './App.css';
 import ToDo from './components/ToDo';
+import ToDoInfo from './components/ToDoInfo';
+
 function App() {
   return (
 
     <div className="App">
       <h1>we here</h1>
       <ToDo />
+      <ToDoInfo />
     </div>
     
     
--- todo/src/components/ToDo.js
+++ todo/src/components/ToDo.js
@@ -3,25 +3,98 @@ import { useState } from "react";
 
 
 function ToDo() {
-    const [doIt, setDoIt] = useState('')
+    const [toDoForm, setToDoForm] = useState({
+        do: "", 
+    })
+
+    function handleChange(e) {
+        setToDoForm((previousFormState) => ({
+            ...previousFormState,
+            do: e.target.value
+        }))
+    }
+    
+    // interacting with my database
+    async function handleSubmit(e) {
+        // prevent reloading
+        e.preventDefault();
+        try{
+            const myToDo = await fetch("http://localhost:4000/do", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(toDoForm)
+            })
+            console.log(myToDo)
+        } catch(err){
+            console.log(err)
+        }
+    }
+
+
     return (
 <>
-<form>
-    <label>
-        <input type ="text"  placeholder="What do you need to do?"
-            value={doIt}
-            onChange={(e) => setDoIt(e.target.value)} />
-        <input type ="submit" text="do it" />
-    </label>  
-        
-        
+<section>
+
+<form onSubmit={handleSubmit}>
+    <input 
+        type ="text" 
+        value={toDoForm.do}
+        placeholder="What do you need to do?"
+        onChange={handleChange}
+        />
+    <input type ="submit" value="do it" />
+  
 </form>
+ 
+</section>
+
 
 </>
         
         
     
     )
-}
+};
+
+// const handleChange = (e) => {
+//     setToDoForm({...toDoForm, [e.target.do]: e.target.value});
+// };
+// const createToDoInfo = async (ToDoInfo) => {
+//     try {
+
+//         // make post request to create people
+// const newPerson = await fetch("http://localhost:4000/do", {
+//     method: "post",
+//     headers: {
+//         "Content-Type": "application/json",
+//     },
+//         body: JSON.stringify(ToDoInfo),
+// });
+
+//         // testing API create request
+//     // console.log(await newPerson.json())
+
+//         // trigger fetch of updated People to replace stale content
+//         getToDoInfo()
+
+//     } catch (err) {
+//         console.log(err)
+        
+//     }
+
+// };
+
+// const handleSubmit = async (e) => {
+    
+// 		e.preventDefault()
+//     const newPerson = await createPeople()
+
+// 		// reset the form
+//     setNewForm({ name: "", image: "", title: "" })
+// }
+
+
 
 export default ToDo;
